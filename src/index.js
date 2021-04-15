230c944cd68cc5af95096d341095ce4df5e91366
Seth Blanchard <nsethblanchard@gmail.com>
2021-04-15 15:53:10 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  
+
+
+const input = document.querySelector('#new-task-description');
+const form = document.querySelector('form');
+
+form.addEventListener('submit', function(event){
+  const ol = document.querySelector('#tasks')
+  let li = document.createElement('li');
+  li.textContent = input.value;
+  ol.append(li);
+  input.value = null;
+  event.preventDefault();
+});
 });
